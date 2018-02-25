b858db7c1543cd5a5960c0827518ea81cae253a9
Tim Whiting <tim@whitings.org>
2018-02-24 22:36:23 -0700

    removed files that were in gitignore

--- ipynb/.ipynb_checkpoints/Ingredient-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import json
-class Ingredient:
-    def __init__(self,name="",amount="1"):
-        self.name = name
-        self.amount = amount
-        
-    def getJSON(self):
-        return json.dumps(self,default=lambda o: o.__dict__,sort_keys=True,indent=4);
