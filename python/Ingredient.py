071b334c3d79904fa99c60614be6739706672437
TimWhiting <tim@whitings.org>
2018-02-26 15:19:29 -0700

    updates to gitignore and scraper as well as putting data into the database:

--- python/Ingredient.py
+++ python/Ingredient.py
@@ -1,8 +1,15 @@
+from bson.json_util import loads
 import json
+
+class IngredientValue:
+    def __init__(self,name="",amount="1"):
+        self.name = name
+        self.amount = amount
+
 class Ingredient:
     def __init__(self,name="",amount="1"):
         self.name = name
         self.amount = amount
         
     def getJSON(self):
-        return json.dumps(self,default=lambda o: o.__dict__,sort_keys=True,indent=4);
+        return loads(json.dumps(self,default=lambda o: o.__dict__))
